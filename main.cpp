cd30e5fb209448791deaab6c76326cd7eec73b44
reality_user <jc3124567@gmail.com>
2023-10-01 15:50:04 -0500

    refactored player controls into Player.control

--- main.cpp
+++ main.cpp
@@ -10,6 +10,13 @@
 
 Vector2 screenSize { 1400, 1050 };
 
+std::tuple<float, float> normalize_vec(float max_velo, float x_velo, float y_velo) {
+  float total_velo = std::abs(x_velo) + std::abs(y_velo);
+  float norm_factor = (total_velo > 10.0) ? std::abs(max_velo) / (total_velo) : 1.0f;
+  return { x_velo * norm_factor, y_velo * norm_factor };
+}
+
+
 class Laserbeam {
 public:
   constexpr static float width { 10.0f };
@@ -38,6 +45,52 @@ public:
   Player(float x, float y) : x(x), y(y), x_velocity(0), y_velocity(0),
     angle(90.0f), acceleration(0.1f), max_velocity(10.0f), laser_cooldown(0),
     laser_recharge_time(20), lasers() {}
+  void control() {
+    if (IsKeyDown(KEY_Q)) angle += 2.0;
+    if (IsKeyDown(KEY_E)) angle -= 2.0;
+
+
+    float next_x_velo { 0 };
+    float next_y_velo { 0 };
+    bool key_pressed { false };
+    if (IsKeyDown(KEY_W)) {
+      next_x_velo = x_velocity + std::cos(angle * PI/180.0) * acceleration;
+      next_y_velo = y_velocity - std::sin(angle * PI/180.0) * acceleration;
+      key_pressed = true;
+    } 
+    if (IsKeyDown(KEY_A)) {
+      next_x_velo = x_velocity - std::sin(angle * PI/180.0) * acceleration;
+      next_y_velo = y_velocity - std::cos(angle * PI/180.0) * acceleration;
+      key_pressed = true;
+    }
+    if (IsKeyDown(KEY_S)) {
+      next_x_velo = x_velocity - std::cos(angle * PI/180.0) * acceleration;
+      next_y_velo = y_velocity + std::sin(angle * PI/180.0) * acceleration;
+      key_pressed = true;
+    }
+    if (IsKeyDown(KEY_D)) {
+      next_x_velo = x_velocity + std::sin(angle * PI/180.0) * acceleration;
+      next_y_velo = y_velocity + std::cos(angle * PI/180.0) * acceleration;
+      key_pressed = true;
+    }
+
+    if (key_pressed) {
+      std::tie(x_velocity, y_velocity) = normalize_vec(max_velocity, next_x_velo, next_y_velo);
+    }
+
+    if (!laser_cooldown && IsKeyDown(KEY_J)) {
+      lasers.push_back(
+        Laserbeam(
+        x + std::cos(angle * PI/180.0) * Laserbeam::width,
+        y - std::sin(angle * PI/180.0) * Laserbeam::width,
+        angle)
+      );
+      laser_cooldown = laser_recharge_time;
+    } else if (laser_cooldown) {
+      --laser_cooldown;
+    }
+
+  }
 };
 
 class Asteroid {
@@ -51,11 +104,6 @@ public:
   Asteroid() : x(0.0f), y(0.0f), x_velocity(1.0f), y_velocity(2.0f), radius(3.0f) {}
 };
 
-std::tuple<float, float> normalize_vec(float max_velo, float x_velo, float y_velo) {
-  float total_velo = std::abs(x_velo) + std::abs(y_velo);
-  float norm_factor = (total_velo > 10.0) ? std::abs(max_velo) / (total_velo) : 1.0f;
-  return { x_velo * norm_factor, y_velo * norm_factor };
-}
 
 template <typename T>
 void update_position(T &object) {
@@ -106,57 +154,22 @@ int main() {
     if (IsKeyDown(KEY_DOWN)) camera.target.y += 3.0f;
 #endif
 
-    float next_x_velo { 0 };
-    float next_y_velo { 0 };
-    bool key_pressed { false };
-    if (IsKeyDown(KEY_W)) {
-      next_x_velo = player.x_velocity + std::cos(player.angle * PI/180.0) * player.acceleration;
-      next_y_velo = player.y_velocity - std::sin(player.angle * PI/180.0) * player.acceleration;
-      key_pressed = true;
-    } 
-    if (IsKeyDown(KEY_A)) {
-      next_x_velo = player.x_velocity - std::sin(player.angle * PI/180.0) * player.acceleration;
-      next_y_velo = player.y_velocity - std::cos(player.angle * PI/180.0) * player.acceleration;
-      key_pressed = true;
-    }
-    if (IsKeyDown(KEY_S)) {
-      next_x_velo = player.x_velocity - std::cos(player.angle * PI/180.0) * player.acceleration;
-      next_y_velo = player.y_velocity + std::sin(player.angle * PI/180.0) * player.acceleration;
-      key_pressed = true;
-    }
-    if (IsKeyDown(KEY_D)) {
-      next_x_velo = player.x_velocity + std::sin(player.angle * PI/180.0) * player.acceleration;
-      next_y_velo = player.y_velocity + std::cos(player.angle * PI/180.0) * player.acceleration;
-      key_pressed = true;
-    }
+    player.control();
 
-    if (key_pressed) {
-      std::tie(player.x_velocity, player.y_velocity) = normalize_vec(player.max_velocity, next_x_velo, next_y_velo);
-    }
-
-    std::cout << "x_velocity: " << player.x_velocity << " y_velocity: " << player.y_velocity << " abs(total): " << std::abs(player.x_velocity) + std::abs(player.y_velocity) << "\n";
+    //std::cout << "x_velocity: " << player.x_velocity << " y_velocity: " << player.y_velocity << " abs(total): " << std::abs(player.x_velocity) + std::abs(player.y_velocity) << "\n";
 
     // player.x += player.x_velocity;
     // player.y += player.y_velocity;
+
+
     update_position(player);
 
-    if (IsKeyDown(KEY_Q)) player.angle += 2.0;
-    if (IsKeyDown(KEY_E)) player.angle -= 2.0;
-    
-    if (!player.laser_cooldown && IsKeyDown(KEY_J)) {
-      player.lasers.push_back(
-        Laserbeam(
-        player.x + std::cos(player.angle * PI/180.0) * Laserbeam::width,
-        player.y - std::sin(player.angle * PI/180.0) * Laserbeam::width,
-        player.angle)
-      );
-      player.laser_cooldown = player.laser_recharge_time;
-    } else if (player.laser_cooldown) {
-      --player.laser_cooldown;
-    }
+
 
     std::erase_if(player.lasers, [/*&screenSize*/](Laserbeam &l){
       return l.x < 0 || l.x > screenSize.x || l.y < 0 || l.y > screenSize.y; });
+
+
     for (auto &l : player.lasers) {
       l.x += std::cos(l.angle * PI/180.0) * l.speed;
       l.y -= std::sin(l.angle * PI/180.0) * l.speed;
