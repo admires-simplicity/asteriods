72c56882fdd437c486e73ed1ce739a2a09f02ed8
reality_user <jc3124567@gmail.com>
2023-10-01 15:24:03 -0500

    added Asteroid object and update_position and screenwrap template functions

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 
 #define DDDD_DEBUG
 
+Vector2 screenSize { 1400, 1050 };
+
 class Laserbeam {
 public:
   constexpr static float width { 10.0f };
@@ -38,16 +40,42 @@ public:
     laser_recharge_time(20), lasers() {}
 };
 
+class Asteroid {
+public:
+  constexpr static float max_velocity {10.0f};
+  float x;
+  float y;
+  float x_velocity;
+  float y_velocity;
+  float radius;
+  Asteroid() : x(0.0f), y(0.0f), x_velocity(1.0f), y_velocity(2.0f), radius(3.0f) {}
+};
+
 std::tuple<float, float> normalize_vec(float max_velo, float x_velo, float y_velo) {
   float total_velo = std::abs(x_velo) + std::abs(y_velo);
   float norm_factor = (total_velo > 10.0) ? std::abs(max_velo) / (total_velo) : 1.0f;
   return { x_velo * norm_factor, y_velo * norm_factor };
 }
 
+template <typename T>
+void update_position(T &object) {
+  object.x += object.x_velocity;
+  object.y += object.y_velocity;
+}
+
+template <typename T>
+void screenwrap(T &object) {
+  if (object.x > screenSize.x) object.x -= screenSize.x;
+  else if (object.x < 0.0f) object.x = screenSize.x - object.x;
+
+  if (object.y > screenSize.y) object.y -= screenSize.y;
+  else if (object.y < 0.0f) object.y = screenSize.y - object.y;
+}
+
+
 int main() {
   std::cout << __func__ << '\n';
 
-  Vector2 screenSize { 1400, 1050 };
   InitWindow(screenSize.x, screenSize.y, "GAME !!!");
 
   Player player{ screenSize.x/2, screenSize.y/2 };
@@ -56,6 +84,8 @@ int main() {
   camera.offset = {screenSize.x/2, screenSize.y/2};
   camera.target = camera.offset;
   camera.zoom = 1.0f;
+
+  Asteroid asteroid;
 
 
   SetTargetFPS(60);
@@ -106,8 +136,9 @@ int main() {
 
     std::cout << "x_velocity: " << player.x_velocity << " y_velocity: " << player.y_velocity << " abs(total): " << std::abs(player.x_velocity) + std::abs(player.y_velocity) << "\n";
 
-    player.x += player.x_velocity;
-    player.y += player.y_velocity;
+    // player.x += player.x_velocity;
+    // player.y += player.y_velocity;
+    update_position(player);
 
     if (IsKeyDown(KEY_Q)) player.angle += 2.0;
     if (IsKeyDown(KEY_E)) player.angle -= 2.0;
@@ -124,21 +155,21 @@ int main() {
       --player.laser_cooldown;
     }
 
-    std::erase_if(player.lasers, [&screenSize](Laserbeam &l){
+    std::erase_if(player.lasers, [/*&screenSize*/](Laserbeam &l){
       return l.x < 0 || l.x > screenSize.x || l.y < 0 || l.y > screenSize.y; });
     for (auto &l : player.lasers) {
       l.x += std::cos(l.angle * PI/180.0) * l.speed;
       l.y -= std::sin(l.angle * PI/180.0) * l.speed;
     }
 
+    
+
+    screenwrap(player);
+
+    screenwrap(asteroid);
 
 
-    if (player.x > screenSize.x) player.x -= screenSize.x;
-    else if (player.x < 0.0f) player.x = screenSize.x - player.x;
-
-    if (player.y > screenSize.y) player.y -= screenSize.y;
-    else if (player.y < 0.0f) player.y = screenSize.y - player.y;
-
+    update_position(asteroid);
 
 
     rlPushMatrix();
@@ -165,6 +196,7 @@ int main() {
       rlPopMatrix();
     }
     
+    DrawCircle(asteroid.x, asteroid.y, asteroid.radius, BLACK);
 
     EndMode2D();
     EndDrawing();
